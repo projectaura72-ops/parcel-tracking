abb1e7031206b517a482d5e4dd342aabb1c29076
aura <auraproject72@gmail.com>
2026-06-24 18:02:19 +0300

    Add Firebase startup logging

--- backend/services/firebase.js
+++ backend/services/firebase.js
@@ -1,12 +1,17 @@
 const admin = require('firebase-admin');
 const config = require('../config');
 
-admin.initializeApp({
-  credential: admin.cert({
-    projectId: config.firebase.projectId,
-    privateKey: config.firebase.privateKey,
-    clientEmail: config.firebase.clientEmail,
-  }),
+const hasProjectId = !!config.firebase.projectId;
+const hasPrivateKey = !!config.firebase.privateKey;
+const hasClientEmail = !!config.firebase.clientEmail;
+console.log('Firebase config:', { hasProjectId, hasPrivateKey, hasClientEmail, projectId: config.firebase.projectId });
+
+const credential = admin.cert({
+  projectId: config.firebase.projectId,
+  privateKey: config.firebase.privateKey,
+  clientEmail: config.firebase.clientEmail,
 });
+
+admin.initializeApp({ credential });
 
 module.exports = admin;
